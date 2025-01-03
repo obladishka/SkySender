ca3d76b379a1acc259d46c5bd98413101703160b
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-01-03 23:23:56 +0300

    add logic for automatic mailing status update

--- mailings/services.py
+++ mailings/services.py
@@ -1,4 +1,5 @@
 import smtplib
+from datetime import datetime
 
 from django.core.mail import send_mail
 from django.shortcuts import redirect
@@ -19,6 +20,8 @@ class MailingService:
         message = mailing.message.message
         recipients = [recipient.email for recipient in mailing.recipients.all()]
 
+        start_at = datetime.now()
+
         try:
             response = send_mail(subject, message, None, recipients, fail_silently=False)
         except smtplib.SMTPException as e:
@@ -26,6 +29,8 @@ class MailingService:
         else:
             MailingService.log_attempt(status="successful", response=response, mailing=mailing)
         finally:
+            end_at = datetime.now()
+            MailingService.update_mailing_status(mailing=mailing, start_at=start_at, end_at=end_at)
             return redirect(reverse("mailings:mailing_list"))
 
     @staticmethod
@@ -33,3 +38,15 @@ class MailingService:
         """Функция для создания записи о попытке рассылки."""
         attempt = Attempt.objects.create(status=status, server_response=response, mailing=mailing)
         attempt.save()
+
+    @staticmethod
+    def update_mailing_status(mailing, start_at, end_at):
+        """Функция для автоматического обновления сведений о рассылке."""
+        if not mailing.start_at and not mailing.end_at:
+            mailing.start_at = start_at
+            mailing.end_at = end_at
+            mailing.status = "finished"
+        elif not mailing.start_at and mailing.end_at:
+            mailing.start_at = start_at
+            mailing.status = "started"
+        mailing.save()
